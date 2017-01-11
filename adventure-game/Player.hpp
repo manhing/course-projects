aa3fca92fa1a762ce02a307b7c03b2d764ba04f7
manhing <leima@onid.oregonstate.edu>
2017-01-10 23:48:01 -0800

    migrating files

---
+++ adventure-game/Player.hpp
@@ -0,0 +1,64 @@
+/**********************************************************************
+ * Description: Header file for Player class for Adventure Game program
+ * *******************************************************************/
+
+
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <string>
+#include <vector>
+
+class Player
+{
+	private:
+		int maxInventoryWeight;
+		int currentInventoryWeight;
+		std::vector<std::string> items;
+
+		//objectives and dependencies to check to see if command can be executed or if is game over
+		bool fireOut;
+		bool engineFueled;
+		bool shipStarted;
+		bool alienKilled;
+		bool foodEaten;
+		bool spaceSuitOn;
+		bool antidoteTaken;
+
+	public:
+		//constructor
+		Player();
+		
+		//setters
+		void setMaxInventoryWeight(int);
+		void addCurrentInventoryWeight(int);
+		void subtractCurrentInventoryWeight(int);
+		void setFireOut(bool);
+		void setEngineFueled(bool);
+		void setShipStarted(bool);
+		void setAlienKilled(bool);
+		void setFoodEaten(bool);
+		void setSpaceSuitOn(bool);
+		void setAntidoteTaken(bool);
+
+
+		//getters
+		int getMaxInventoryWeight();
+		int getCurrentInventoryWeight();
+		std::vector<std::string> getItems();
+		bool getFireOut();
+		bool getEngineFueled();
+		bool getShipStarted();
+		bool getAlienKilled();
+		bool getFoodEaten();
+		bool getSpaceSuitOn();
+		bool getAntidoteTaken();
+
+
+		void addItem(std::string);
+		void removeItem(std::string);
+};
+
+
+
+#endif
---
+++ adventure-game/Room.hpp
@@ -0,0 +1,65 @@
+/**********************************************************************
+ * Description: Header file for Room class for Adventure Game program
+ * *******************************************************************/
+
+
+#ifndef ROOM_HPP
+#define ROOM_HPP
+
+#include <string>
+#include <vector>
+
+class Room
+{
+	private:
+		std::string roomName;
+		std::string longDesc;
+		std::string dependentDesc;
+		std::string shortDesc;
+		std::vector<std::string> items;
+		std::vector<std::string> features;
+
+
+		//for navigation, we can test to see if current room has a doorway
+		bool isDoorN;
+		bool isDoorE;
+		bool isDoorS;
+		bool isDoorW;
+
+		bool hasVisited; // for determining whether to print long or short description
+
+	public:
+		//setters
+		void setRoomName(std::string);
+		void setLongDesc(std::string);
+		void setDependentDesc(std::string);
+		void setShortDesc(std::string);
+		void setIsDoorN(bool);
+		void setIsDoorE(bool);
+		void setIsDoorS(bool);
+		void setIsDoorW(bool);
+		void setHasVisited(bool);
+
+		//getters
+		std::string getRoomName();
+		std::string getLongDesc();
+		std::string getDependentDesc();
+		std::string getShortDesc();
+		std::vector<std::string> getItems();
+		std::vector<std::string> getFeatures();
+		bool getIsDoorN();
+		bool getIsDoorE();
+		bool getIsDoorS();
+		bool getIsDoorW();
+		bool getHasVisited();
+		
+		void addItem(std::string);
+		void removeItem(std::string);
+
+		void addFeature(std::string);
+
+};
+
+
+
+#endif
